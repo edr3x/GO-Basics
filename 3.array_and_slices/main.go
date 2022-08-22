3ad15c1fadce80c37ffb29fbb020cf5549c4d2d1
EDR3X <dhunganaannanta@gmail.com>
2022-08-22 15:55:50 +0545

    Array and slices

--- 3.array_and_slices/main.go
+++ 3.array_and_slices/main.go
@@ -5,15 +5,32 @@ import "fmt"
 func main() {
 
 	//	var arr [5]int = [5]int{1, 2, 3, 4, 5}
-	var arr = [...]int{1, 2, 3, 4, 5}
+	var arr = [5]int{1, 2, 3, 4, 5}
 
-	names := [2]string{"jhon", "doe"}
-	names[1] = "dont"
+	names := [4]string{"jhon", "doe", "obi-wan", "kenobi"}
 
 	fmt.Println(arr, len(arr))
 	fmt.Println(names, len(names))
 
-	//* Slices
+	//* Slices(use arrays under the hood)
 
-	// var scores = []int{445, 232, 435}
+	var scores = []int{445, 232, 435}
+	scores[0] = 670
+	fmt.Println(scores, len(scores))
+	scores = append(scores, 85) //* can't do append with array but can do with slices
+
+	fmt.Println(scores, len(scores))
+
+	//slice ranges
+
+	range1 := names[1:3]
+	range2 := names[2:]
+	range3 := names[:3]
+
+	fmt.Println(range1, "\n", range2, "\n", range3)
+
+	range1 = append(range1, "broda")
+
+	fmt.Println(range1)
+
 }
